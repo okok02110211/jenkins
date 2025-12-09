cf9a1191534b5335f102602bd9d5926c79744267
okok02110211 <okok02110211@gmail.com>
2025-12-09 09:18:51 +0900

    .

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive";
+        return "jenkins day2 test";
     }
 
     @GetMapping("/plus")
